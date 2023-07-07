0a847fbb424b81dbfb6ab54b44210f62bfae4e63
Pierre <pierre.petitiot@gmail.com>
2023-07-07 10:06:31 +0200

    07/07/2023

--- src/components/Produits.js
+++ src/components/Produits.js
@@ -5,11 +5,20 @@ const Produits = () => {
 
     const [produitsData, setProduitsData] = useState([]);  // Pour stocker les données des produits
     const [search, setSearch] = useState("");  // Pour stocker la recherche (searchBar)
+    const [searchType, setSearchType] = useState("id");  // Par défaut, recherche par ID de produit
+    let apiUrl = "https://pierre.amorce.org/api/produits";
 
 
     useEffect(() => {
+
+        if (searchType === "id") {  // Si recherche par ID
+            apiUrl += `/${search}`;
+          } else {  // Si recherche par nom
+            apiUrl += `?&nom=${search}`;
+          }
+
         axios
-            .get("https://pierre.amorce.org/api/produits", { headers: { Accept: "application/json" } })  // lien de l'API pour aller chercher tous les produits
+            .get(apiUrl, { headers: { Accept: "application/json" } })  // lien de l'API pour aller chercher tous les produits
             .then((res) => setProduitsData(res.data));
         // Avoir seulement les données des produits :
         // Méthode 1 : Dans res.data --> mettre {headers: {Accept: "application/json"}} dans le .get
@@ -30,28 +39,35 @@ const Produits = () => {
                             id="search-input"
                             onChange={e => setSearch(e.target.value)}  // On stock la recherche dans setSearch
                         />
-                        <input type="submit" value="Rechercher" />
+
+                        <input type="submit" value="Rechercher" /> <br />
+
+                        <label>
+                            <input
+                                type="radio"
+                                value="id"
+                                checked={searchType === "id"}
+                                onChange={() => setSearchType("id")}
+                            />
+                            Recherche par ID
+                        </label>
+
+                        <label>
+                            <input
+                                type="radio"
+                                value="nom"
+                                checked={searchType === "nom"}
+                                onChange={() => setSearchType("nom")}
+                            />
+                            Recherche par nom
+                        </label>
+
                     </form>
                 </div>
             </div>
-            <div className="result">
-                {produitsData
-                    .slice(0, 24)  /* slice(0, 24) --> Pour afficher seulement 24 résultats */
-                    // eslint-disable-next-line
-                    .sort((a, b) => {
-                        if (sortGoodBad === "goodToBad") {
-                            return b.vote_average - a.vote_average  /* On tri du mieux noté au moins bien noté */
-                        }
-                        else if (sortGoodBad === "badToGood") {
-                            return a.vote_average - b.vote_average  /* On tri du moins bien noté au mieux noté */
-                        }
 
-                    })
-                    .map((movie) => {
-                        return (
-                            <Card movie={movie} key={movie.id} />
-                        )
-                    })}
+            <div className="result">
+
             </div>
         </div>
     );
